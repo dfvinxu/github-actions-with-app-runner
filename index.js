0da97f06c78a25dfb37586c68beef04906f01e8d
David Fernandez Joaquin <df.vinxu@gmail.com>
2023-07-13 09:25:15 +0200

    Comprobando deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Running on AWS App Runner Service !');
+    res.send('Running on AWS App Runner Service ! 13 de julio');
 });
 
 const PORT = process.env.PORT || 8080;
